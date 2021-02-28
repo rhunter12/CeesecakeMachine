933d8c69aba13a0e5af1ba52ac985e4362450935
ryanhunter <bliss22@up.edu>
2021-02-28 08:12:19 -0800

    Update to json test file

--- hw3/tests/json.test.js
+++ hw3/tests/json.test.js
@@ -1,16 +1,17 @@
+//Ryan Hunter-Bliss
+
 const request = require('supertest');
 const express = require('express');
 
 const app = express();
 
-
-
-
-
 test('checks that json is returned', () => {
+    //verify that json is returned
     app.get('/user', function(req, res) {
         res.status(200).json({ topping: 'cherry' });
     });
+
+    //check the json to make sure it is correct
     request(app)
     .get('/user')
     .expect('Content-Type', /json/)
